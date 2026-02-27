36d524ca285b472bded2597d2ad8f72124b27b2a
Amirhassan Adeli <amirhassanadeli.ai@gmail.com>
2026-02-28 02:29:52 +0330

    updatefiles

--- src/model_training.py
+++ src/model_training.py
@@ -1,3 +1,4 @@
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -21,6 +22,8 @@ x_test = scaler.transform(x_test)
 
 model = LinearRegression()
 model.fit(x_train, y_train)
+
+joblib.dump(model, "../models/model.pkl")
 
 y_pred = model.predict(x_test)
 
